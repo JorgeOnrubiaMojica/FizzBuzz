3cf9d42fe7a6b072fb6a213007babdc7b9da9fb9
Jorge Onrubia <jorge.onrubia.mojica@gmail.com>
2021-11-13 09:35:43 +0100

    Baby steps

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,6 +1,12 @@
 class FizzBuzz {
     fun getOutput(number: String): String {
-        return if (number.toInt() == 1) number else "Fizz"
+        return if (number.toInt() == 1){
+            number
+        } else if (number.toInt() == 3){
+            "Fizz"
+        } else{
+            "Buzz"
+        }
 
     }
 }
--- src/test/kotlin/FizzBuzzShould.kt
+++ src/test/kotlin/FizzBuzzShould.kt
@@ -1,17 +1,28 @@
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-internal class FizzBuzzShould {
+internal class FizzBuzzShould() {
+
+    private lateinit var fizzBuzz: FizzBuzz
+
+    @BeforeEach
+    fun createFizzBuzz(){
+        this.fizzBuzz = FizzBuzz()
+    }
 
     @Test
     fun `returns the number if the number is 1`(){
-        val fizzBuzz = FizzBuzz()
         assertEquals("1", fizzBuzz.getOutput("1"))
     }
 
     @Test
     fun `return Fizz if number is divisible by 3`(){
-        val fizzBuzz = FizzBuzz()
         assertEquals("Fizz", fizzBuzz.getOutput("3"))
+    }
+
+    @Test
+    fun `return FizzBuzz if number is divisible by 5`(){
+        assertEquals("Buzz", fizzBuzz.getOutput("5"))
     }
 }
